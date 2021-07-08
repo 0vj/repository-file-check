9e75944b42cfe81a88204e6ddce677ca6ffcc8db
Amirmahdi <tafreshi.amirmahdi@protonmail.com>
2021-07-08 14:22:43 +0430

    Debug

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mr-tafreshi/repo-file-check
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.2 // indirect
+	github.com/go-git/go-git/v5 v5.4.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,50 +1,54 @@
 package main
 
 import (
-	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-git/go-git"
+	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 )
 
 func main() {
 	r := gin.Default()
-	r.POST("/api/v1/validate_repository", validate_repository)
+	r.POST("/api/v1/validate_repository", ValidateRepositoryEndpoint)
 	r.Run()
 }
 
-func ValidateRepositoryEndpoint(c *gin.Context):
-        repository := c.PostForm("repository")
-        username := c.PostForm("username")
-        password := c.PostForm("password")
+func ValidateRepositoryEndpoint(c *gin.Context) {
+	repository := c.PostForm("repository")
+	username := c.PostForm("username")
+	password := c.PostForm("password")
 	validate_status, have_err := ValidateRepository(repository, username, password)
-	if have_err true {
-                c.JSON(200, gin.H{
-                        "status":  false,
-                        "messege": "There was a problem checking the repository",
-                })
+	if have_err == true {
+		c.JSON(200, gin.H{
+			"status":  false,
+			"messege": "There was a problem checking the repository",
+		})
 	} else {
-		if validate_staus {
-                        c.JSON(200, gin.H{
-                                "status": true,
-                                "Repository approved"
-                        })
+		if validate_status {
+			c.JSON(200, gin.H{
+				"status":  true,
+				"messege": "Repository approved",
+			})
 		} else {
-                        c.JSON(200, gin.H{
-                                "status":  false,
-                                "messege": "Repository not approved",
-                        })
-                }
+			c.JSON(200, gin.H{
+				"status":  false,
+				"messege": "Repository not approved",
+			})
+		}
 	}
+}
 
-
-func ValidateRepository(repository, username, password string) (bool, bool){
+func ValidateRepository(repository, username, password string) (bool, bool) {
 	if username != "" && password == "" {
-		_, err = git.PlainClone("./tmp", false, &git.CloneOptions{
+		_, err := git.PlainClone("./tmp", false, &git.CloneOptions{
 			URL:      repository,
 			Progress: os.Stdout,
 		})
+		if err != nil {
+			return false, true
+		}
+		println(err)
 	} else {
 		_, err := git.PlainClone("./tmp", false, &git.CloneOptions{
 			URL:      repository,
@@ -54,17 +58,18 @@ func ValidateRepository(repository, username, password string) (bool, bool){
 				Password: password,
 			},
 		})
+		println(err)
 		if err != nil {
 			return false, true
 		}
 	}
-	if fileExists("tmp/" + os.Getenv("FILE_VALIDATE")) == true {
+	havevalidatefile := fileExists("tmp/"+os.Getenv("FILE_VALIDATE"))
+	os.RemoveAll("tmp")
+	if havevalidatefile == true {
 		return true, false
 	} else {
 		return false, false
 	}
-	os.RemoveAll("tmp")
-
 }
 
 func fileExists(filename string) bool {
